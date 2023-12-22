d402c362d495f603bbcb4f5a111d2b230c08a631
pushpander1010 <pushpanderkmr@gmail.com>
2023-12-22 10:21:21 +0530

    commit_2023_12_22_10-21-21

---
+++ src/dsa/concept/LinkedListConcept.java
@@ -0,0 +1,19 @@
+package dsa.concept;
+
+public class LinkedListConcept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
+
+class Nodel{
+	int data;
+	Nodel next;
+	
+	Nodel(Nodel next,int data){
+		this.next=next;
+		this.data=data;
+	}
+}
